f454e430a28c77a3f8a9a7e034260a8867d52b41
Madeline Morton <madelinemorton614@gmail.com>
2019-05-22 14:36:42 -0500

    base mode done

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,20 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if (req.isAuthenticated()){
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    let queryText = `SELECT * FROM "secret"
+                WHERE $1>"secrecy_level";`;
+    
+    pool.query(queryText, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
